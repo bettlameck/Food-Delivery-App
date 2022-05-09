e33d680468c0fecf70ea64b9e0573bb6806681df
kiprutobrian <briankipruto793@gmail.com>
2022-05-09 09:15:36 +0300

    IconAndTextWidget

--- lib/widgets/icon_and_text_widget.dart
+++ lib/widgets/icon_and_text_widget.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/cupertino.dart';
+import 'package:food_delivery/widgets/small_text.dart';
 
 class IconAndTextWidget extends StatelessWidget {
   final IconData icon;
   final String text;
-  final Color color;
   final Color iconColor;
   const IconAndTextWidget({
     Key? key,
-    this.icon,
-    this.text,
-    this.color,
-    this.iconColor,
+    required this.icon,
+    required this.text,
+    required this.iconColor,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Icon(icon, color: iconColor,),
+        SizedBox(width: 5),
+        SmallText(text: text)
+      ],
+    );
   }
 }
