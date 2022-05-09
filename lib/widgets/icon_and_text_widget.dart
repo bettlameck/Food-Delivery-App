114f937bc53ec29d0853b9a172fc1fe9b603742b
bettlameck <lamequebett@gmail.com>
2022-05-09 09:02:26 +0300

    IconAndTextWidget

---
+++ lib/widgets/icon_and_text_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+
+class IconAndTextWidget extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final Color color;
+  final Color iconColor;
+  const IconAndTextWidget({
+    Key? key,
+    this.icon,
+    this.text,
+    this.color,
+    this.iconColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
